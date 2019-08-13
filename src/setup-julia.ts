f8e69caa034309eb278311be76739cca2eda4a58
Sascha Mann <git@mail.saschamann.eu>
2019-08-13 13:03:00 +0200

    Use tool-cache to install Julia

--- src/setup-julia.ts
+++ src/setup-julia.ts
@@ -1,32 +1,33 @@
-import * as core from "@actions/core";
-import * as exec from "@actions/exec";
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as tc from '@actions/tool-cache'
 
-import * as os from "os";
+import * as os from 'os'
+import * as path from 'path'
 
 async function run() {
-  try {
-    const version = core.getInput("version");
-    core.debug(`selected Julia version: ${version}`);
+    try {
+        const version = core.getInput('version')
+        console.log(`[DEBUG] selected Julia version: ${version}`)
 
-    // Store information about the environment
-    const osPlat = os.platform();
-    const osArch = os.arch();
+        // Store information about the environment
+        const osPlat = os.platform()
+        const osArch = os.arch()
 
-    // For now, just download Linux x64 binaries
-    await exec.exec("curl", [
-      "-O",
-      "https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.1-linux-x86_64.tar.gz"
-    ]);
-    await exec.exec("tar xf julia-1.0.1-linux-x86_64.tar.gz");
+        // For now, just download Linux x64 binaries and add it to PATH
+        const juliaDownloadPath = await tc.downloadTool(
+            'https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.4-linux-x86_64.tar.gz'
+        )
+        const juliaExtractedFolder = await tc.extractTar(juliaDownloadPath)
+        const juliaCachedPath = await tc.cacheDir(juliaExtractedFolder, 'julia', '1.0.4', 'x64')
+        const juliaPath = path.join(juliaCachedPath, 'julia-1.0.4')
+        core.addPath(path.join(juliaPath, 'bin'))
 
-    // Add the downloaded binary to path
-    core.addPath("julia-1.0.1/bin");
-
-    // Test if it worked
-    await exec.exec("julia", ["--version"]);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        // Test if it worked
+        await exec.exec('julia', ['-e', 'using InteractiveUtils; versioninfo()'])
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
-run();
+run()
