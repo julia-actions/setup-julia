3440ae5aa52c48603a84106358ac3f4ecdc23ca9
Sascha Mann <git@mail.saschamann.eu>
2020-02-07 18:22:26 +0100

    Move version matching tests

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -39,6 +39,45 @@ process.env['RUNNER_TEMP'] = tempDir;
 
 import * as installer from '../src/installer'
 
+describe('version matching tests', () => {
+    describe('specific versions', () => {
+        it('Doesn\'t change the version when given a valid semver version', async () => {
+            expect(await installer.getJuliaVersion([], '1.0.5')).toEqual('1.0.5')
+            expect(await installer.getJuliaVersion(['v1.0.5', 'v1.0.6'], '1.0.5')).toEqual('1.0.5')
+            expect(await installer.getJuliaVersion(['v1.0.4', 'v1.0.5'], '1.0.5')).toEqual('1.0.5')
+            expect(await installer.getJuliaVersion(['v1.0.4'], '1.0.5')).toEqual('1.0.5')
+            expect(await installer.getJuliaVersion([], '1.3.0-alpha')).toEqual('1.3.0-alpha')
+            expect(await installer.getJuliaVersion(['v1.2.0', 'v1.3.0-alpha', 'v1.3.0-rc1', 'v1.3.0'], '1.3.0-alpha')).toEqual('1.3.0-alpha')
+            expect(await installer.getJuliaVersion([], '1.3.0-rc2')).toEqual('1.3.0-rc2')
+        })
+
+        it('Doesn\'t change the version when given `nightly`', async () => {
+            expect(await installer.getJuliaVersion([], 'nightly')).toEqual('nightly')
+            expect(await installer.getJuliaVersion(testVersions, 'nightly')).toEqual('nightly')
+        })
+    })
+
+    describe('version ranges', () => {
+        it('Chooses the highest available version that matches the input', async () => {
+            expect(await installer.getJuliaVersion(testVersions, '1')).toEqual('1.2.0')
+            expect(await installer.getJuliaVersion(testVersions, '1.0')).toEqual('1.0.5')
+            expect(await installer.getJuliaVersion(testVersions, '^1.3.0-rc1')).toEqual('1.3.0-rc4')
+            expect(await installer.getJuliaVersion(testVersions, '^1.2.0-rc1')).toEqual('1.2.0')
+        })
+    })
+
+    describe('node-semver behaviour', () => {
+        describe('Windows installer change', () => {
+            it('Correctly understands >1.4.0', () => {
+                expect(semver.gtr('1.4.0-rc1', '1.3', {includePrerelease: true})).toBeTruthy()
+                expect(semver.gtr('1.4.0-DEV', '1.3', {includePrerelease: true})).toBeTruthy()
+                expect(semver.gtr('1.3.1', '1.3', {includePrerelease: true})).toBeFalsy()
+                expect(semver.gtr('1.3.2-rc1', '1.3', {includePrerelease: true})).toBeFalsy()
+            })
+        })
+    })
+})
+
 describe('installer tests', () => {
     beforeAll(async () => {
         await io.rmRF(toolDir)
@@ -53,42 +92,6 @@ describe('installer tests', () => {
             console.log('Failed to remove test directories')
         }
     }, 100000)
-
-    describe('version matching', () => {
-        describe('specific versions', () => {
-            it('Doesn\'t change the version when given a valid semver version', async () => {
-                expect(await installer.getJuliaVersion([], '1.0.5')).toEqual('1.0.5')
-                expect(await installer.getJuliaVersion(['v1.0.5', 'v1.0.6'], '1.0.5')).toEqual('1.0.5')
-                expect(await installer.getJuliaVersion(['v1.0.4', 'v1.0.5'], '1.0.5')).toEqual('1.0.5')
-                expect(await installer.getJuliaVersion(['v1.0.4'], '1.0.5')).toEqual('1.0.5')
-                expect(await installer.getJuliaVersion([], '1.3.0-alpha')).toEqual('1.3.0-alpha')
-                expect(await installer.getJuliaVersion(['v1.2.0', 'v1.3.0-alpha', 'v1.3.0-rc1', 'v1.3.0'], '1.3.0-alpha')).toEqual('1.3.0-alpha')
-                expect(await installer.getJuliaVersion([], '1.3.0-rc2')).toEqual('1.3.0-rc2')
-            })
-            it('Doesn\'t change the version when given `nightly`', async () => {
-                expect(await installer.getJuliaVersion([], 'nightly')).toEqual('nightly')
-                expect(await installer.getJuliaVersion(testVersions, 'nightly')).toEqual('nightly')
-            })
-        })
-        describe('version ranges', () => {
-            it('Chooses the highest available version that matches the input', async () => {
-                expect(await installer.getJuliaVersion(testVersions, '1')).toEqual('1.2.0')
-                expect(await installer.getJuliaVersion(testVersions, '1.0')).toEqual('1.0.5')
-                expect(await installer.getJuliaVersion(testVersions, '^1.3.0-rc1')).toEqual('1.3.0-rc4')
-                expect(await installer.getJuliaVersion(testVersions, '^1.2.0-rc1')).toEqual('1.2.0')
-            })
-        })
-    })
-    describe('node-semver behaviour', () => {
-        describe('Windows installer change', () => {
-            it('Correctly understands >1.4.0', () => {
-                expect(semver.gtr('1.4.0-rc1', '1.3', {includePrerelease: true})).toBeTruthy()
-                expect(semver.gtr('1.4.0-DEV', '1.3', {includePrerelease: true})).toBeTruthy()
-                expect(semver.gtr('1.3.1', '1.3', {includePrerelease: true})).toBeFalsy()
-                expect(semver.gtr('1.3.2-rc1', '1.3', {includePrerelease: true})).toBeFalsy()
-            })
-        })
-    })
 
     describe('versions.json parsing', () => {
         // Instead of downloading versions.json, use fixtures/versions.json
