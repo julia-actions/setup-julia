152b85e98236044b24a978f387752d16117b52df
スノル <s@sunoru.com>
2023-02-09 18:00:27 -0500

    Update dependencies (#133)
    * Use `fs.rmSync` instead of `fs.rmdirSync`.
    
    To get rid of the deprecation warning. #119
    
    * Update dependencies.

--- src/setup-julia.ts
+++ src/setup-julia.ts
@@ -76,7 +76,7 @@ async function run() {
             core.debug(`added Julia to cache: ${juliaPath}`)
 
             // Remove temporary dir
-            fs.rmdirSync(juliaInstallationPath, {recursive: true})
+            fs.rmSync(juliaInstallationPath, {recursive: true})
         } else {
             core.debug(`using cached version of Julia: ${juliaPath}`)
         }
@@ -91,7 +91,7 @@ async function run() {
         const showVersionInfoInput = core.getInput('show-versioninfo')
         await installer.showVersionInfo(showVersionInfoInput, version)
     } catch (error) {
-        core.setFailed(error.message)
+        core.setFailed((error as Error).message)
     }
 }
 
